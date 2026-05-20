0680f16fa0606487b9c88826a823219c4cbf3335
Peter Bernanth <peterbernanth@gmail.com>
2026-05-20 22:02:11 +0530

    metro config update

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,16 @@
-const { getDefaultConfig } = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 const defaultConfig = getDefaultConfig(__dirname);
+const { assetExts, sourceExts } = defaultConfig.resolver;
 
-defaultConfig.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
-defaultConfig.resolver.assetExts = defaultConfig.resolver.assetExts.filter(ext => ext !== 'svg');
-defaultConfig.resolver.sourceExts.push('svg');
+const config = {
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  },
+  resolver: {
+    assetExts: assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
+  },
+};
 
-module.exports = defaultConfig;
+module.exports = mergeConfig(defaultConfig, config);
